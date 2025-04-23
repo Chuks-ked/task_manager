b279b147dd0226ecb6fbf179b155d15eab5036b5
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-04-23 22:50:44 +0100

    router urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import UserCreateView, UserProfileView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserCreateView, UserProfileView, TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='signup'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('', include(router.urls)),
 ]
