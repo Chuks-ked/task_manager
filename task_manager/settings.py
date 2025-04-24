b415df069e98f1a089d5a169d5b2d20dbc8c109b
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-04-24 19:15:46 +0100

    celery settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -155,3 +155,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
