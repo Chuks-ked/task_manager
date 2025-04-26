fa305ab7c3554b831de19d64cd2c08e8d541c291
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-04-26 10:04:39 +0100

    customadmin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Task
 
-admin.site.register(User)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('-date_joined',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('username',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+
+    
 admin.site.register(Task)
