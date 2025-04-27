4e0960c239555929a3cb4990798e5cb3d8e96a9b
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-04-27 21:38:58 +0100

    throtthle classes per requests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -185,6 +185,17 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',  # 100 requests per day for anonymous users
+        'user': '1000/day',  # 1000 requests per day for authenticated users
+    },
 }
 
 
